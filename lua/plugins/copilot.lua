24db7699c774ebaa773486e2b403fbc65f9def98
Silver <silver178@github.com>
2026-03-18 13:44:58 -0700

    Added LSPconfig/Mason support for easier crosscompatibility

--- lua/plugins/copilot.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"github/copilot.vim",
-	lazy = false,
-	config = function()
-		vim.g.copilot_no_tab_map = true
-		vim.g.copilot_assume_mapped = true
-	end,
-}
---
+++ lua/plugins/mason.lua
@@ -0,0 +1,27 @@
+--local clangd = require("lsp.clangd")
+return {
+	"mason.nvim",
+	{
+		"mason-org/mason-lspconfig.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { ensure_installed = "lua_ls", "clangd", "omnisharp" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({})
+			lspconfig.omnisharp.setup({})
+			lspconfig.clangd.setup()
+		end,
+	},
+}
